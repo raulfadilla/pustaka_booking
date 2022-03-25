37447885cb0fba079d3731f999a5cf6b786bdc23
raulfadilla <raulfdl19@gmail.com>
2022-03-25 19:32:10 +0700

    menambahkan file latihan1.php,model_latihan1.php,view_latihan1.php

---
+++ application/views/view_latihan1.php
@@ -0,0 +1,13 @@
+<html>
+
+<Head>
+    <title>Latihan 1</title>
+</Head>
+
+<body>
+    Semuanya yok berhitung <br>
+    Nilai 1 = <?= $nilai1; ?>
+    Nilai 2 = <?= $nilai2; ?>
+    ini hasil dari penjumlahan <?= $nilai1 . "+" . $nilai2 . "=" . $hasil; ?>
+</body>
+</html>
